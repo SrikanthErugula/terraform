147baba8c5ae128854f0ba6a121b3a4edf1ed436
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-12-23 10:12:24 +0530

    terraform

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -1,3 +1,39 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "6.16.0"
+#     }
+#   }
+
+
+# # terraform {
+# #   backend "s3" {
+# #     bucket = "mybucket"
+# #     key    = "path/to/my/key"
+# #     region = "us-east-1"
+# #   }
+# # }
+
+#   backend "s3" {
+#     bucket = "dsoaws-remote-state"
+#     key    = "remote-state-demo" # key must be need set for storing state file ikkada ichhina name tho state file will be storing
+#     region = "us-east-1"
+#     use_lockfile = true  # it used to lock the state file when someone useing same time
+#     #dynamodb_table = "terraform-locks"
+#     encrypt = true
+#   }
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# # so here backend ni configure cheyakapothe state file ni local lo peduthundhi..
+
+# # adhe backed ni configure cheste adhi velli s3 bucket lo store avuthundhi...
+
+
 terraform {
   required_providers {
     aws = {
@@ -6,21 +42,11 @@ terraform {
     }
   }
 
-
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "path/to/my/key"
-#     region = "us-east-1"
-#   }
-# }
-
   backend "s3" {
     bucket = "dsoaws-remote-state"
-    key    = "remote-state-demo" # key must be need set for storing state file ikkada ichhina name tho state file will be storing
+    key    = "remote-state-demo"
     region = "us-east-1"
-    #use_lockfile = true  # it used to lock the state file when someone useing same time
-    #dynamodb_table = "terraform-locks"
+    use_lockfile = true
     encrypt = true
   }
 }
@@ -28,7 +54,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# so here backend ni configure cheyakapothe state file ni local lo peduthundhi..
-
-# adhe backed ni configure cheste adhi velli s3 bucket lo store avuthundhi...
