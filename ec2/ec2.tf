17edd96cb40c8b4eaf5cfb20a2b55f337ebfe66c
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-21 11:57:52 +0530

    terraform_start

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "tfstart" {
-  ami           = "ami-09c813fb71547fc4f"
+  ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg_tfstart_allow.id] # see in notes
 
