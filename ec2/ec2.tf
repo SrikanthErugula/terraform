78570f5a6333057d6b739fb86b0fa0a008b04663
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-22 07:18:11 +0530

    terraform_start

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -4,14 +4,14 @@ resource "aws_instance" "tfstart" {
   vpc_security_group_ids = [aws_security_group.sg_tfstart_allow.id] # see in notes
 
   tags = {
-    Name = "tfstart"
+    Name = "terraform_start"
     terraform = true # it is used to know like terrform tho create chesa ani cheptundi true ani vunte 
   }
 } # so upto here instances or server create avuthai based on above code 
 
 # It is like as a block
 resource "aws_security_group" "sg_tfstart_allow" {
-  name   = "sg_terraform_allow" # names anevi alredy exist ayyi vunte delete chesi or vere names ivvali
+  name   = "sg_terra_allow" # names anevi alredy exist ayyi vunte delete chesi or vere names ivvali
   #vpc_id = aws_vpc.example.id # no need bcz it will take direct it from default
 
 # It is like as a block
