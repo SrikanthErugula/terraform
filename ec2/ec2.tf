ce4c0a2916f873466ed49963542dd708c64d9de1
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-06-01 12:12:54 +0530

    modified_code

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "tfstart" {
+resource "aws_instance" "tf_re" {
   ami           =  "ami-0220d79f3f480ecf5" #"ami-09c813fb71547fc4f" 
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.sg_tfstart.id] # see in notes
+  vpc_security_group_ids = [aws_security_group.sg.id] # see in notes
 
   tags = {
-    Name = "terraform_start"
+    Name = "terraform-tag"
     terraform = true # it is used to know like terrform tho create chesa ani cheptundi true ani vunte 
   }
 } # so upto here instances or server create avuthai based on above code 
 
 # It is like as a block
-resource "aws_security_group" "sg_tfstart" {
-  name   = "sg_tf_allow" # names anevi alredy exist ayyi vunte delete chesi or vere names ivvali
+resource "aws_security_group" "sg_re" {
+  name   = "sg_name" # names anevi alredy exist ayyi vunte delete chesi or vere names ivvali
   #vpc_id = aws_vpc.example.id # no need bcz it will take direct it from default
 
 # It is like as a block
@@ -32,7 +32,7 @@ resource "aws_security_group" "sg_tfstart" {
   }
 
   tags = {
-    Name = "sg_terraform_allow"
+    Name = "sg-tag-name"
   }
 }
 # within {} we called as map or object 
